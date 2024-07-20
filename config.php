5bd66cdb5d9bb0421f430df802e73c044470e4d7
Guillermo Rojas <apemangr@gmail.com>
2024-07-20 18:48:10 -0400

    Nueva configuración

--- config.php
+++ config.php
@@ -1,13 +1,21 @@
-<?php
+// Configuración de conexión a la base de datos
 $host = 'localhost';
 $username = 'root';
 $password = '';
 $dbname = 'AGENCIA';
 
-try{
-    $conn = new PDO("mysql:host=$host;dbname$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conecado a la base de datos de AGENCIA";
-} catch (PDOException $e){
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    $conn->exec("set names utf8");
+    
+    echo "Conectado a la base de datos de AGENCIA";
+
+} catch (PDOException $e) {
     echo "Error al conectar: " . $e->getMessage();
 }
+
+$conn = null;
+?>
